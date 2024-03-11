74f5e7891f56b7fe2edaeaa4025e62e8f264046e
Abdul Manaf <Sahitoa97@Gmail.com>
2024-03-11 15:12:14 +0500

    test files and overall connected

--- Segmentation/Segmentation-2/Mask2Former/test.py
+++ Segmentation/Segmentation-2/Mask2Former/test.py
@@ -9,7 +9,13 @@ from transformers import (
 from custom_datasets import get_images, get_dataset, get_data_loaders
 from model import load_model
 from config import ALL_CLASSES, LABEL_COLORS_LIST
-from engine import validate
+from engine import validate, validate_each_class
+
+seed = 42
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -35,8 +41,7 @@ print(args)
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     processor = Mask2FormerImageProcessor(ignore_index=255, reduce_labels=True)
-    model = Mask2FormerForUniversalSegmentation.from_pretrained(args.model) 
-    model.to(device).eval()
+    model = Mask2FormerForUniversalSegmentation.from_pretrained(args.model).to(device)
     
     test_images, test_masks = get_images(root_path='input/road_seg', test=True)
     test_dataset = get_dataset(
@@ -66,7 +71,7 @@ if __name__ == '__main__':
     metric = evaluate.load("mean_iou")
 
     # Testing
-    valid_epoch_loss, test_miou = validate(
+    test_loss, test_miou = validate(
         model,
         test_dataloader,
         device,
