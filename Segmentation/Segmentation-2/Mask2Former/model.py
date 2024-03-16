e81b125c57e147f0a87d33990e7963f981458037
Abdul Manaf <Sahitoa97@Gmail.com>
2024-03-17 00:34:14 +0500

    changes

--- Segmentation/Segmentation-2/Mask2Former/model.py
+++ Segmentation/Segmentation-2/Mask2Former/model.py
@@ -6,9 +6,13 @@ def load_model(num_classes=1):
     image_processor = Mask2FormerImageProcessor(ignore_index=255, reduce_labels=True)
 
     model = Mask2FormerForUniversalSegmentation.from_pretrained(
-        'facebook/mask2former-swin-tiny-ade-semantic',
+        'facebook/mask2former-swin-large-ade-semantic',
         num_labels=num_classes,
         ignore_mismatched_sizes=True
     )
 
     return model, image_processor
+
+#  'facebook/mask2former-swin-tiny-ade-semantic'
+#  'facebook/mask2former-swin-large-ade-semantic'
+# https://huggingface.co/facebook?search_models=mask2former-swin-large
